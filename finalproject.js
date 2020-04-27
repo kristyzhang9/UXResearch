503bc1e4701e487d8c8f37c522b24b25a3dee6d7
Kristy Zhang <kristyzhang@Kristys-MBP.attlocal.net>
2020-04-26 20:38:01 -0700

    second commit

---
+++ finalproject.js
@@ -0,0 +1,23 @@
+function gotowhatisit(url) {
+    window.location = url;
+
+
+function gotomethods(url) {
+    window.location = url;
+}
+
+function button1click (url){
+	window.location.href = 'url';
+}
+
+function button2click (url){
+	window.location.href = 'url';
+}
+
+function button3click (url){
+	window.location.href = 'url';
+}
+
+AOS.init({
+			duration: 2000,
+		});
