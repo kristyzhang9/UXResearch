843729697fd3acc7d603ddbaf9745f29a205089f
Kristy Zhang <kristyzhang@Kristys-MBP.attlocal.net>
2020-05-05 19:05:17 -0700

    fourth commit

--- Assets/JS/finalproject.js
+++ Assets/JS/finalproject.js
@@ -1,23 +1,9 @@
+//buttons on home page//
 function gotowhatisit(url) {
     window.location = url;
-
+}
 
 function gotomethods(url) {
     window.location = url;
 }
 
-function button1click (url){
-	window.location.href = 'url';
-}
-
-function button2click (url){
-	window.location.href = 'url';
-}
-
-function button3click (url){
-	window.location.href = 'url';
-}
-
-AOS.init({
-			duration: 2000,
-		});
